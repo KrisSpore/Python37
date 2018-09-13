797ce8a594456d18b12757a2d59ef2d0bdc2e978
KrisSpore <43150823+KrisSpore@users.noreply.github.com>
2018-09-13 19:27:12 +0200

    dodaje plik

---
+++ Numbers.py
@@ -0,0 +1,30 @@
+numer = 3
+
+numer_str = "3"
+
+print("3")
+
+print (numer)
+
+print(type(3))
+print(type("3"))
+
+print(type(3.14))
+
+print(0.1+0.1+0.1)
+
+print("True")
+
+print(type("True"))
+
+numer = numer + 1
+
+print (numer)
+
+print(numer ** 2)
+print (numer/3)
+
+print (numer//3)
+
+print(4%2)
+
---
+++ hello.py
@@ -0,0 +1,40 @@
+# komentarz jednoliliniowy
+#
+
+print ('Siemandero!')
+
+numer = 3
+
+numer_str = "3"
+
+print("3")
+
+print (numer)
+
+print(type(3))
+print(type("3"))
+
+print(type(3.14))
+
+print(0.1+0.1+0.1)
+
+print("True")
+
+print(type("True"))
+
+numer = numer + 1
+
+print (numer)
+
+
+halo = "Halo!"
+
+kuzyn = "kuzyn"
+
+lacznie = halo + kuzyn
+
+print(lacznie)
+
+name = input ("Jak masz na imię?   ")
+
+print ("Milo Cię poznać " + name)
