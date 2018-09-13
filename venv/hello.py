81718fe22ace813de32330e7e54dd00ff55709a6
KrisSpore <43150823+KrisSpore@users.noreply.github.com>
2018-09-13 18:31:53 +0200

    dodaje plik

---
+++ venv/hello.py
@@ -0,0 +1,20 @@
+# komentarz jednoliliniowy
+#
+
+print ('Siemandero!')
+
+print("3")
+
+print (3)
+
+print(type(3))
+print(type("3"))
+
+print(type(3.14))
+
+print(0.1+0.1+0.1)
+
+print("True")
+
+print(type("True"))
+
